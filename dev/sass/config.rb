785d61245f71e09d2739998271edfe25e50c5df6
Пётр <volkov_p_v@mail.ru>
2014-11-04 03:04:13 +0300

    Rejected commit: Default

--- dev/sass/config.rb
+++ dev/sass/config.rb
@@ -9,7 +9,7 @@ sass_dir = ""
 images_dir = "../../www/image"
 
 # You can select your preferred output style here (can be overridden via the command line):
- output_style = :expanded
+ output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
