9f8802cbdc3c1ff6bcda81e1778ccd996cd06c7b
Пётр <volkov_p_v@mail.ru>
2014-11-04 03:09:08 +0300

    Rejected commit: Default

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,9 @@ var serveStatic      = require('serve-static');
 var concat           = require('gulp-concat');
 var gulpFilter       = require('gulp-filter');
 var mainBowerFiles   = require('main-bower-files');
-var cssmin			 = require('gulp-cssmin');
 var uglify 			 = require('gulp-uglify');
 var rename           = require('gulp-rename');
-var prefix    		 = require('gulp-autoprefixer');
 var unhtml           = require('gulp-minify-html');
-var spritesmith      = require('gulp.spritesmith');
 
 // мини-сервер для livereLoad
 gulp.task('server', function(next) {
@@ -47,17 +44,6 @@ gulp.task('libs', function () {
 
 });
 
-// css. Добавляем или удаляем автопрефикс и минифицируем.
-//gulp.task('css', function() {
-//    return gulp.src('dev/sass/style.css')
-//        .pipe(prefix(["last 5 version", "ie 8", "ie 7"]))
-//        .pipe(cssmin())
-//        .pipe(rename({
-//            suffix: ".min"
-//        }))
-//        .pipe(gulp.dest('www/style/'));
-//});
-
 // js. Склеиваем и минифицируем
 gulp.task('js', function () {
     return gulp.src('dev/js/*.js')
@@ -78,36 +64,15 @@ gulp.task('minifyHtml', function() {
         .pipe(gulp.dest('www/'))
 });
 
-// Создаем спрайт
-// gulp.task('sprite', function () {
-    // var spriteData = gulp.src('dev/sprite/*.png')
-        // .pipe(spritesmith({
-            // imgName: 'sprite.png',
-            // cssFormat: 'sass',
-            // cssName: 'sprite.sass',
-            // padding: 10,
-            // algorithm: 'binary-tree',
-            // cssVarMap: function(sprite) {
-                // sprite.name = 'sprite-' + sprite.name
-            // }
-        // }));
-    // spriteData.img.pipe(gulp.dest('www/image/'));
-    // spriteData.css.pipe(gulp.dest('dev/sass/library/'));
-// });
-
 // а теперь наблюдаем за происходящим =)
 gulp.task('watch', ['server'], function() {
     var server = livereLoad();
 
     gulp.watch('bower.json',['libs']);
 
-    //gulp.watch('dev/sass/style.css',['css']);
-
     gulp.watch('dev/js/*.js',['js']);
 
     gulp.watch('dev/*.html',['minifyHtml']);
-
-    //gulp.watch('dev/sprite/*.png',['sprite']);
 
     gulp.watch('www/**/*').on('change', function(file) {
         server.changed(file.path);
